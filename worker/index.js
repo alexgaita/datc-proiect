125ff24928516916e4b623b30d2b16a29d4ee374
alexandrugaita <alex.gaita99@gmail.com>
2023-11-25 13:29:53 +0200

    added worker to save statistscs every day at 10

---
+++ worker/index.js
@@ -0,0 +1,34 @@
+const { TableClient } = require("@azure/data-tables");
+const { v4: uuidv4 } = require('uuid');
+require('dotenv').config(); // Load environment variables from .env file
+
+const connectionString = process.env.CONNECTION_STRING;
+
+// Create a new instance of the TableClient
+const zoneTableClient = TableClient.fromConnectionString(connectionString, "ambroziaZones");
+const statisticsTableClient = TableClient.fromConnectionString(connectionString, "statistics");
+
+const getAllZonesSize = async () => {
+
+    const zoneReponse = zoneTableClient.listEntities();
+    let size = [];
+
+    for await (const zone of zoneReponse) {
+        size += zone.size;
+    }
+
+    return size;
+}
+
+const createStatistics = async () => {
+    const size = await getAllZonesSize();
+    const statistics = {
+        partitionKey: uuidv4(),
+        rowKey: "Statistics",
+        size: size
+    }
+
+    statisticsTableClient.createEntity(statistics);
+}
+
+createStatistics().then(() => console.log("Statistics created"));
