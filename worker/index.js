ebe1d3847deb734d880d62012c3dd7f7d9e6cf48
alexandrugaita <alex.gaita99@gmail.com>
2023-11-27 21:13:55 +0200

    fix worker

--- worker/index.js
+++ worker/index.js
@@ -1,34 +1,39 @@
 const { TableClient } = require("@azure/data-tables");
-const { v4: uuidv4 } = require('uuid');
-require('dotenv').config(); // Load environment variables from .env file
+const { v4: uuidv4 } = require("uuid");
+require("dotenv").config(); // Load environment variables from .env file
 
 const connectionString = process.env.CONNECTION_STRING;
 
 // Create a new instance of the TableClient
-const zoneTableClient = TableClient.fromConnectionString(connectionString, "ambroziaZones");
-const statisticsTableClient = TableClient.fromConnectionString(connectionString, "statistics");
+const zoneTableClient = TableClient.fromConnectionString(
+  connectionString,
+  "ambroziaZones"
+);
+const statisticsTableClient = TableClient.fromConnectionString(
+  connectionString,
+  "statistics"
+);
 
 const getAllZonesSize = async () => {
+  const zoneReponse = zoneTableClient.listEntities();
+  let size = 0;
 
-    const zoneReponse = zoneTableClient.listEntities();
-    let size = [];
+  for await (const zone of zoneReponse) {
+    size += Number(zone.size);
+  }
 
-    for await (const zone of zoneReponse) {
-        size += zone.size;
-    }
-
-    return size;
-}
+  return size;
+};
 
 const createStatistics = async () => {
-    const size = await getAllZonesSize();
-    const statistics = {
-        partitionKey: uuidv4(),
-        rowKey: "Statistics",
-        size: size
-    }
+  const size = await getAllZonesSize();
+  const statistics = {
+    partitionKey: uuidv4(),
+    rowKey: "Statistics",
+    size: size,
+  };
 
-    statisticsTableClient.createEntity(statistics);
-}
+  statisticsTableClient.createEntity(statistics);
+};
 
 createStatistics().then(() => console.log("Statistics created"));
